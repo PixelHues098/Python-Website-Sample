c03b12c983c0823a595d531a811c593e5485595f
PixelHues098 <mobiemocken0978@gmail.com>
2025-03-01 09:10:16 +0800

    made some progress in the webpage

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "This is the home page"
+    return "HELLO 2E!"
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
--- view.py
+++ view.py
@@ -1,4 +1,8 @@
 from flask import Blueprint
 
 
-views = Blueprint("views")
+views = Blueprint("views") 
+
+@views.route("/"):
+def home():
+    return "home page"
